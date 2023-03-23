ec020e2a4a35ac05934997d8c5e2e2d6e541c6c6
unknown <david.narvaez@labinfo.is.escuelaing.edu.co>
2023-03-23 13:32:14 -0500

    first update

---
+++ src/main/java/org/example/HelloWorld.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import static spark.Spark.*;
+
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        //API: secure(keystoreFilePath, keystorePassword, truststoreFilePath,truststorePassword);
+        secure(getKeyStore(), getPwdKeyStore(), null, null);
+        port(getPort());
+        get("/hello", (req, res) -> "Hello World");
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 5000; //returns default port if heroku-port isn't set (i.e. on localhost)
+    }
+    private static String getKeyStore() {
+        if (System.getenv("KEYSTORE") != null) {
+            return System.getenv("KEYSTORE");
+        }
+        return "certificados/ecikeystore.p12";
+    }
+    private static String getPwdKeyStore() {
+        if (System.getenv("KEYSTOREPWD") != null) {
+            return System.getenv("KEYSTOREPWD");
+        }
+        return "123456";
+    }
+//keytool -genkeypair -alias ecikeypair -keyalg RSA -keysize 2048 -storetype PKCS12 -keystore ecikeystore.p12 -validity 3650
+//https://localhost:5000/hello
+}
